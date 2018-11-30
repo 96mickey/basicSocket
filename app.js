6976126327592d5cae605020eef69c9edcb483a2
Mayank <mayankworklabs@gmail.com>
2018-11-30 18:27:57 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+var io = require("socket.io").listen(app.listen(5000));
+
+io.sockets.on("connection", function(socket) {
+  socket.emit("Start_Chat");
+
+  socket.on("hitting", function(data) {
+    io.sockets.emit("hitting_event", { key: data.key });
+  });
+});
+
+// io.sockets.on("connection", function(socket) {});
